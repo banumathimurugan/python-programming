35724fd2ae80445dae9ebf214a4760d52e7b5c3a
banumathimurugan <banufrisbee@gmail.com>
2018-02-23 14:21:07 +0530

    Create vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,6 @@
+x=raw_input("Enter the letter")
+if( x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or
+x=='A' or x=='E' or x=='I' or x=='o' or x=='u'):
+  print("letter is vowel")
+else:
+  print("letter is consonant")
