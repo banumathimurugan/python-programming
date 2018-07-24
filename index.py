1498dff788cffe61ec8482b30f2a729085f4226e
banumathimurugan <banufrisbee@gmail.com>
2018-07-24 12:06:43 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+a=raw_input()
+length=len(a)
+for i in range(0,lenght):
+    if(a[i]=="k"):
+        print a[i]
+        break
+     elif(a[i]=="k"):
+        print a[i]
+        break
+     else:
+        print a[i]
