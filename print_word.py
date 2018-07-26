0e5067726d91c78eebd9e37fed66e09cfb5ce7cb
banumathimurugan <banufrisbee@gmail.com>
2018-07-26 16:17:17 +0530

    Create print_word.py

---
+++ print_word.py
@@ -0,0 +1,8 @@
+def main():
+       try:
+             d=['zero','one','two','three','four','five','six','seven','eight',''nine','ten']
+             x=int(input())
+   print(d[x])
+       except:
+              print('invalid')
+main()
